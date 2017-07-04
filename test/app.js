65e2ea90f4603abd3cf9da3c51d1e03f11df160c
rakeshr99 <rakeshr99@ccs.neu.edu>
2017-07-04 18:51:13 -0400

    adding mongo string app.js

--- test/app.js
+++ test/app.js
@@ -10,7 +10,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds135552.mlab.com:35552/heroku_2zbnqrsz'; // user yours
+	
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
